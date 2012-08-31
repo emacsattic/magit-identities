1a411098ad7b2108d632595dbc533b242b95a598
Sébastien Gross <sebchezwamorg>
2012-08-31 22:21:38 +0200

    Add autoload to add-hook.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- magit-identities.el
+++ magit-identities.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2012-08-30
-;; Last changed: 2012-08-31 10:36:28
+;; Last changed: 2012-08-31 22:21:03
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -67,6 +67,7 @@ Order matters: first matched identity is used."
    (magit-identities-get-id
     (magit-identities-get-repo-string))))
 
+;;;###autoload
 (add-hook 'magit-log-edit-mode-hook 'magit-identities-set-id)
 
 ;; magit-identities.el ends here
